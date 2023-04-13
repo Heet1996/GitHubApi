9c20ca4ca56777b885d22846c4bf3b7ea922d37c
Thue Andersen <t.udengaardandersen@elsevier.com>
2023-04-13 13:12:54 +0200

    Fixing code imports

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import {createStore,applyMiddleware,compose, combineReducers} from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import './index.css';
 import App from './App';
@@ -10,7 +11,9 @@ import * as serviceWorker from './serviceWorker';
 import tokenReducer from './store/reducers/tokenValidator';
 import repoReducer from './store/reducers/repoSearch';
 
-const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = composeWithDevTools({
+    // Specify here name, actionsDenylist, actionsCreators and other options
+});
 let rootReducer=combineReducers({tokenValidator:tokenReducer,repoReducer:repoReducer});
 const store=createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
 
